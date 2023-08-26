65de5b98ea4f046ca15bafa29cd12e7e28fa2979
marabidze <marabidze@slb.com>
2023-08-26 22:04:05 +0400

    feat: add locations service

---
+++ src/app/services/locations/locations.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+
+type location = {
+  country: string;
+  cities: string[];
+};
+type locationsResponse = {
+  data: location[];
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationsService {
+  constructor(private http: HttpClient) {}
+
+  getAllCities(): Observable<string[]> {
+    const url = 'https://countriesnow.space/api/v0.1/countries';
+    return this.http
+      .get<locationsResponse>(url)
+      .pipe(map((locations) => locations.data.map((loc) => loc.cities).flat()));
+  }
+}
