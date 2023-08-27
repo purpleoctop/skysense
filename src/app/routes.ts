da6332542ae7cda038882c7a7999fb383543fcb1
marabidze <marabidze@slb.com>
2023-08-27 10:08:36 +0400

    fix: routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
 ];
 
 export default routes;
